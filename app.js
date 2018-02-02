225ab1f63379c7581a831b17f15d9b26f6ffa760
cross <cross.network.software@gmail.com>
2018-02-02 10:46:17 +0700

    delete system

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const url       = require('url')
 const routes    = require('routes')()
 const view      = require('swig')
 const fs        = require('fs')
+const mysql     = require('mysql')
+const qs        = require('querystring')
+// config mysql
+let con = mysql.createConnection({
+    host:"localhost",
+    user:'root',
+    password:'',
+    database:'crossnet'
+})
 
 // assets
 routes.addRoute("/style.css", function(req,res){
@@ -12,32 +21,80 @@ routes.addRoute("/style.css", function(req,res){
         res.end()
     })
 })
+routes.addRoute("/datetime.css", function(req,res){
+    fs.readFile("view/assets/css/datetime.css",function(err,data){
+        res.writeHead(200,{'Content-Type':'text/css'})
+        res.write(data)
+        res.end()
+    })
+})
 
-
-
+routes.addRoute("/datetime.full.js", function(req,res){
+    fs.readFile("view/assets/js/datetime.full.js",function(err,data){
+        res.writeHead(200,{'Content-Type':'text/js'})
+        res.write(data)
+        res.end()
+    })
+})
 
 // route
 routes.addRoute("/login",function(req,res){
     html  = view.compileFile('./view/auth/index.html')()
-    
     res.writeHead(200,{'Content-Type' : 'text/html'})
     res.end(html)
 })
 
-routes.addRoute("/",function(req,res){
-    let title = ['zulfikra','lahmudin','rifaizal']
-    html  = view.compileFile('./view/home/index.html')({
-        title:title,
-    })  
-    res.writeHead(200,{'Content-Type' : 'text/html'})
-    res.end(html)
+routes.addRoute("/dialy",function(req,res){
+        if(req.method.toUpperCase() == "GET"){
+            con.query("SELECT * FROM dialy_activity", function(err, result, field){  
+                html  = view.compileFile('./view/home/index.html')({
+                    data:result
+                })  
+                res.writeHead(200,{'Content-Type' : 'text/html'})
+                res.end(html)
+            }) 
+        }
+        
+        if(req.method.toUpperCase() == 'POST'){
+            var formData = ""
+            req.on('data',function(chunck){
+               formData += chunck
+            })
+            req.on("end", function(){              
+                var data    = qs.parse(formData.toString())  
+                var sql     = `INSERT INTO dialy_activity (nama,keterangan,status,start_datetime,end_datetime,alamat,user_id) VALUES ('${data.nama}','${data.keterangan}','${data.status}','${data.start_datetime}','${data.end_datetime}','${data.alamat}',1)`
+                con.query(sql,function(err,result){
+                    if(err) throw err
+                    res.writeHead(302,{
+                        'Location':'/dialy'
+                    })
+                    res.end()   
+                }) 
+                    
+            })
+            
+        }
+
 })
 
+routes.addRoute("/dialy/delete/",function(req,res){
+    if(req.method.toUpperCase() == 'POST'){
+        var formData = ""
+        req.on('data',function(chunk){
+            formData += chunk
+            
+        })
 
-
-routes.addRoute("/detail/:name",function(req,res){
-    res.writeHead(200,{'Content-Type' : 'text/plain'})
-    res.end("hello " + this.params.name)
+        req.on('end', function(){
+            var data = qs.parse(formData.toString())
+            var sql = `DELETE FROM dialy_activity WHERE id=${data.id}`
+            con.query(sql, function(err,result){
+                res.writeHead(302,{'Location' : '/dialy'})
+                res.end()
+            })
+            
+        })
+    }
 })
 
 
